b946271e98b2e13a0cb2161d248d5f5972826e11
MubeenAli-ri <mubeenaliop1@gmail.com>
2026-05-18 05:50:46 +0500

    Added Exception Files

--- backend/src/exception/BackupNotFoundException.java
+++ backend/src/exception/BackupNotFoundException.java
@@ -1,5 +1,15 @@
 package com.backuptool.exception;
 
-public class BackupNotFoundException {
+public class BackupNotFoundException extends RuntimeException {
 
+    private final String backupId;
+
+    public BackupNotFoundException(String backupId) {
+        super("Backup record not found with ID: " + backupId);
+        this.backupId = backupId;
+    }
+
+    public String getBackupId() {
+        return backupId;
+    }
 }
--- backend/src/exception/BackupOperationException.java
+++ backend/src/exception/BackupOperationException.java
@@ -1,5 +1,20 @@
 package com.backuptool.exception;
 
-public class BackupOperationException {
+public class BackupOperationException extends RuntimeException {
 
+    private final String operationType;
+
+    public BackupOperationException(String operationType, String message) {
+        super("[" + operationType.toUpperCase() + "] " + message);
+        this.operationType = operationType;
+    }
+
+    public BackupOperationException(String operationType, String message, Throwable cause) {
+        super("[" + operationType.toUpperCase() + "] " + message, cause);
+        this.operationType = operationType;
+    }
+
+    public String getOperationType() {
+        return operationType;
+    }
 }
